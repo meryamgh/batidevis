6f8e3b4930b6ea63b9b50456f1e10667f1387ddd
Amel <naloufi.amel@gmail.com>
2025-03-15 10:54:07 +0100

    modifs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import FullQuote from './pages/FullQuote';
-import Main from './pages/Main';
 import Home from './pages/Home';
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Main />} />
             <Route path="/maquette" element={<MainPage />} />
             <Route path="/full-quote" element={<FullQuote />} />
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
         </Routes>
     );
 }
